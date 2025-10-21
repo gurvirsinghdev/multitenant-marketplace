9c7ee064850cd2fccceaa1607c31e8c9274004e1
Gurvir Singh <gurvirsingh.dev@gmail.com>
2025-10-21 16:10:59 +0530

    feat: added home layout

---
+++ app/(home)/navbar-sidebar.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+
+import { NavbarItemRaw } from "./navbar";
+
+interface Props {
+  items: NavbarItemRaw[];
+  onOpenChange: (open: boolean) => void;
+  open: boolean;
+}
+
+export const NavbarSidebar = function ({
+  items,
+  onOpenChange,
+  open,
+}: Props) {
+  const pathname = usePathname();
+
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent
+        side="left"
+        className="p-0 transition-none"
+      >
+        <SheetHeader className="p-4 border-b">
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              onClick={() => onOpenChange(false)}
+              className={cn(
+                "w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium",
+                pathname === item.href &&
+                  "bg-black text-white",
+              )}
+            >
+              {item.children}
+            </Link>
+          ))}
+          <div className="border-t">
+            <Link
+              href="/sign-in"
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+            >
+              Login in
+            </Link>
+            <Link
+              href="/sign-up"
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+            >
+              Start selling
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
